ace607b8472dcbe33cbd30ddcc5cf939fc8b091b
sojeong0302 <01duqrl@naver.com>
2024-01-02 21:37:10 +0900

    feat : 배경화면

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
+import BackGround from "./component/BackGround.js";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BackGround />
+    </div>
+  );
 }
 export default App;
---
+++ src/component/BackGround.js
@@ -0,0 +1,12 @@
+import BackGroundImg from "../img/BackGroundImg.png";
+import "./BackGround.css";
+
+const BackGround = () => {
+  return (
+    <div>
+      <img alt="배경화면" src={BackGroundImg} />
+    </div>
+  );
+};
+
+export default BackGround;

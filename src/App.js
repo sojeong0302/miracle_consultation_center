3e02929036dbf757c4634585cd757eb02c4d570d
sojeong0302 <01duqrl@naver.com>
2024-01-02 21:44:51 +0900

    style : 배경화면 css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
-import BackGround from "./component/BackGround.js";
 
 function App() {
-  return (
-    <div className="App">
-      <BackGround />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 export default App;

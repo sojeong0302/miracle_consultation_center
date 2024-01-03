d7b8916e7d18e4e49ebf7b7a29def549eedc9d42
sojeong0302 <01duqrl@naver.com>
2024-01-03 22:47:24 +0900

    feat : Pagination & mockData 가져오기

--- src/App.js
+++ src/App.js
@@ -6,14 +6,71 @@ function App() {
     {
       id: 1,
       nickName: "소정",
-      date: new date().getTime(),
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 0,
+    },
+    {
+      id: 2,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 1,
+    },
+    {
+      id: 3,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 1,
+    },
+    {
+      id: 4,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 1,
+    },
+    {
+      id: 5,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 1,
+    },
+    {
+      id: 6,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 0,
+    },
+    {
+      id: 7,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 0,
+    },
+    {
+      id: 8,
+      nickName: "소정",
+      date: "2023-01-01",
+      content: "mock1",
+      isChecked: 0,
+    },
+    {
+      id: 9,
+      nickName: "소정",
+      date: "2023-01-01",
       content: "mock1",
       isChecked: 0,
     },
   ];
+
   return (
     <div className="App">
-      <WriteList />
+      <WriteList mockData={mockData} />
     </div>
   );
 }
--- src/page/WriteList.js
+++ src/page/WriteList.js
@@ -1,9 +1,34 @@
-const WriteList = () => {
+import React, { useState } from "react";
+import { Checkbox, Pagination } from "@mui/material";
+
+const WriteList = ({ mockData }) => {
+  const itemsPerPage = 8;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = mockData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleChangePage = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
+
   return (
-    <div className="writeList-container">
-      <h1>"답변을 기다리는 상담이 5개 있습니다."</h1>
-      <div className="list">상담 리스트</div>
-      <div>페이지네이션</div>
+    <div className="writeList-c ontainer">
+      <h1>"답변을 기다리는 상담이 10개 있습니다."</h1>
+      <div className="list">
+        {currentItems.map(({ id, nickName, date, content, isChecked }) => (
+          <div key={id}>
+            {id} {nickName} {date} {content}
+            <Checkbox checked={isChecked === 1} />
+          </div>
+        ))}
+      </div>
+      <Pagination
+        count={Math.ceil(mockData.length / itemsPerPage)}
+        page={currentPage}
+        onChange={handleChangePage}
+      />
     </div>
   );
 };
